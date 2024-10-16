d9acaac1ae06c9aab14e14c6bd0ea0883d66c27e
Eugene-74 <eugenezrg@gmail.com>
2024-10-16 10:49:30 +0200

    delete .bat

--- compileAndStart.bat
+++
@@ -1,8 +0,0 @@
-fpc main.pas
-fpc types.pas
-fpc gestion.pas
-
-
-@REM fpc -Fu/home/eugene/Documents/git/SDL2-for-Pascal/units SDLImageDisplay.pas
-
-main
--- pull-push.bat
+++
@@ -1,4 +0,0 @@
-git pull origin eugene
-git add .
-git commit -m "update"
-cmd /K git push origin eugene
