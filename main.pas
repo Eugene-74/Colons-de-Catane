5b3e9c2517cb4ea17995e048795b8e8dc0b6cf8d
Renardo-7 <eugenezrg@gmail.com>
2024-12-12 13:12:07 +0100

    propre

--- main.pas
+++ main.pas
@@ -8,16 +8,7 @@ begin
   randomize;  
   initialisationPartie(joueurs,plateau,affichage);
   
-  joueurs[0].Ressources[Mathematiques] := 99;
-  joueurs[0].Ressources[Chimie] := 99;
-  joueurs[0].Ressources[Informatique] := 99;
-  joueurs[0].Ressources[Humanites] := 99;
-  joueurs[0].Ressources[Physique] := 99;
-
-  joueurs[1].Ressources[Mathematiques] := 99;
-  joueurs[1].Ressources[Chimie] := 99;
-  joueurs[1].Ressources[Informatique] := 99;
-  joueurs[1].Ressources[Humanites] := 99;
-  joueurs[1].Ressources[Physique] := 99;
+  demarrer_musique();
+  
   partie(affichage,joueurs,plateau);
 end.
