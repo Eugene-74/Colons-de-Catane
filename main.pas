164ac6356d09be18c42b23952f7cb292193c1658
Eugene-74 <eugenearg@gmail.com>
2024-12-13 09:26:03 +0100

    bon pour l'oral

--- main.pas
+++ main.pas
@@ -6,12 +6,6 @@ var affichage: TAffichage;
   joueurs: TJoueurs;
 begin
   initialisationPartie(joueurs,plateau,affichage);
-
-  // joueurs[2].Ressources[Mathematiques] := 99;
-  // joueurs[2].Ressources[Chimie] := 99;
-  // joueurs[2].Ressources[Informatique] := 99;
-  // joueurs[2].Ressources[Humanites] := 99;
-  // joueurs[2].Ressources[Physique] := 99;
   
   partie(affichage,joueurs,plateau);
 end.
